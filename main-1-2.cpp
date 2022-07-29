4206cc9fdbd2e73d731370169b6010ac6a6feaf8
SamuelMunaro <110147544+SamuelMunaro@users.noreply.github.com>
2022-07-29 17:31:01 +0930

    Add files via upload

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -6,6 +6,6 @@ extern double array_mean(int[], int);
 
 int main() {
   int array[5] = {1, 2, 3, 4, 5};
-  std::cout << "The average is: " << double array_mean(array, 5) << std::endl;
+  std::cout << "The average is: " << array_mean(array, 5) << std::endl;
   return 0;
 }
