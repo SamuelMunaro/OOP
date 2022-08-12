895d9af3e9d2c3d74541d8c180d238e67d7c7cf3
SamuelMunaro <110147544+SamuelMunaro@users.noreply.github.com>
2022-08-12 16:32:15 +0930

    Add files via upload

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int palindrome_sum(int integers[], int length) {
+    
+    extern bool is_array_palindrome(int integers[], int length);
+    extern int sum_integers(int integers[], int length);
 
     int sum = 1;
 
@@ -8,36 +11,57 @@ int palindrome_sum(int integers[], int length) {
 
         sum = sum + integers[i];
 
-        std::cout << sum << std::endl;
-
     }
 
+    int output = is_array_palindrome(integers, length);
 
-    return sum;
+    if (output != 0) {
+        output = -2;
+    } 
+    if (length == 0) {
+        output = -1;
+    }
+
+    sum_integers(integers, length);
+
+    return output;
 }
 
 bool is_array_palindrome(int integers[], int length) {
 
-    int r = 0;
+    bool r = 0;
 
     for (int i = 0; i < length; i ++) {
 
-        if (i < length/2 && integers[i] <= integers[i+1]) {
+        if ((i < length/2) && (integers[i] >= integers[i+1])) {
 
-            r = 0;
+            r = r;
 
-        } elseif (i > length/2 && integers[i-1] >= integers[i]) {
+        } else if (i >= length/2 && integers[i-1] >= integers[i]) {
             
-            r = 0;
+            r = r;
 
         } else {
 
-            r = -2;
+            r = r + 1;
 
         }
 
     }
 
     return r;
+}
 
+
+int sum_integers(int integers[], int length) {
+    
+    int sum = 0;
+
+    for (int i = 0; i < length; i++) {
+
+        sum = sum + integers[i];
+
+    }
+
+    return sum;
 }
