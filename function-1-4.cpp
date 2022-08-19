eb2254400b8d10514af1d3f51f57e661b8a9c299
SamuelMunaro <110147544+SamuelMunaro@users.noreply.github.com>
2022-08-19 23:33:50 +0930

    Add files via upload

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -11,17 +11,5 @@ void copy_doubles(double *old_array,double *new_array,int length) {
 
     new_array = val;
 
-    for (int i = 0; i < length; i++) {
-
-        std::cout << new_array[i];
-
-        if (i < (length-1)) {
-
-            std::cout << " ";
-
-        }
-
-    }
-
     return;
 }
