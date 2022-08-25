b24b4dc5c839177e0ff73f03389d9c8f5b8d97d7
SamuelMunaro <110147544+SamuelMunaro@users.noreply.github.com>
2022-08-25 14:49:06 +0930

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -4,19 +4,16 @@ void print_ascending(int *vals, int len) {
 
     int out = 1;
 
+    std::cout << vals[0] << ' ';
+
     for (int i = 0; i < len-1; i++) {
 
-        if (vals[i] <= vals[i+1]) {
+        if (vals[i+1] >= vals[i]) {
 
             out++;
 
-            std::cout << vals[i];
+            std::cout << vals[i+1] << ' ';
 
-            if (i < out+1) {
-
-                std::cout << ' ';
-
-            }
 
         }
 
