c0dab93811be1a99a53482e43f327e186173779e
SamuelMunaro <110147544+SamuelMunaro@users.noreply.github.com>
2022-09-08 14:45:54 +0930

    Add files via upload

--- Main-1-2.cpp
+++ Main-1-2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "StoreShelf.h"
-//#include "MusicBox.h"
+#include "MusicBox.h"
 #include <string>
 
 using namespace std;
@@ -29,6 +29,8 @@ int StoreShelf::get_num_music_boxes();
 {
     size / dimension;
 }
+
+MusicBox *get_contents();
 
 // implementation of the default MusicBox destructor
 MusicBox::~MusicBox()
